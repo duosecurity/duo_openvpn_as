c28ce7489e22c05978fc05618ac50f982b93dcfd
Adam Goodman <akgood@duosecurity.com>
2012-06-27 11:32:58 -0400

    remove factor list from challenge; use duo_unix style copyright notice

--- duo_openvpn_as.py
+++ duo_openvpn_as.py
@@ -1,22 +1,9 @@
 #!/usr/bin/env python
 #
 # duo_openvpn_as.py
-# Duo OpenVPN v1
+#
 # Copyright 2012 Duo Security, Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# All rights reserved, all wrongs reversed.
 
 # ------------------------------------------------------------------
 # Fill in your integration credentials on the following three lines:
@@ -173,21 +160,7 @@ def preauth(ikey, skey, host, username):
 
     if result == API_RESULT_AUTH:
         log('secondary authentication required for user %s' % username)
-
-        factors = response.get('factors')
-        if factors is None:
-            log('invalid API response: %s' % response)
-            raise RuntimeError('invalid API response: %s' % response)
-
-        factors_list = []
-        for i in itertools.count(1):
-            factor = factors.get(str(i))
-            if factor is None:
-                break
-            factors_list.append(factor)
-        msg = ('Enter Duo passcode or out-of-band factor (%s):'
-               % ', '.join(factors_list))
-
+        msg = 'Enter Duo passcode or out-of-band factor:'
         return (result, msg)
 
     status = response.get('status')
