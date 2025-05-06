6cea7df0e8d30b428b5bdc54c01c150e578c2800
Aaron McConnell <amcconnell@duosecurity.com>
2025-05-06 16:45:23 -0400

    Version bump to 2.7

--- duo_openvpn_as.py
+++ duo_openvpn_as.py
@@ -903,7 +903,7 @@ class CertValidatingHTTPSConnection(httplib.HTTPConnection):
 
 ### duo_openvpn_as.py integration code:
 
-__version__ = '2.6'
+__version__ = '2.7'
 
 def log(msg):
     msg = 'Duo OpenVPN_AS: %s' % msg
